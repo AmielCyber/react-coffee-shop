ed8b978f8998ea9806c4d438dc912992f3143dc9
Amiel Nava <amieln@icloud.com>
2022-06-08 09:59:08 -0700

    Made useSWR hook reusable since I will be using it to fetch drinks from the menu and fetching past orders for the users account page.

--- components/Drinks/AvailableDrinks.js
+++ components/Drinks/AvailableDrinks.js
@@ -1,38 +1,27 @@
 import React from 'react';
-import useSWR from 'swr';
 // My imports.
+import FetchItems from '../../store/fetcher/fetch-items';
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import DrinkItemList from './DrinkItem/DrinkItemList';
 // CSS import.
 import styles from './AvailableDrinks.module.css';
 
-// Fetcher for the SWR state.
-async function fetcher(url) {
-  const response = await fetch(url);
-  const responseData = await response.json();
-  if (!response.ok) {
-    // Make SWR catch the failed response.
-    throw new Error(responseData.message);
-  }
-  return responseData;
-}
-
 export default function AvailableDrinks() {
-  const { data, error } = useSWR('/api/menu/drinks', fetcher);
+  const { items, isLoading, isError } = FetchItems('/api/menu/drinks');
 
   // Display server error.
-  if (error) {
+  if (isError) {
     return (
       <section className={styles.drinksError}>
         <Card>
-          <h3>{error.message}</h3>
+          <h3>{isError.message}</h3>
         </Card>
       </section>
     );
   }
   // Display loading content since we do not have our data yet.
-  if (!data) {
+  if (isLoading) {
     return (
       <section className={styles.drinksLoading}>
         <Card>
@@ -46,7 +35,7 @@ export default function AvailableDrinks() {
   return (
     <section className={styles.drinks}>
       <Card>
-        <DrinkItemList drinks={data} />
+        <DrinkItemList drinks={items} />
       </Card>
     </section>
   );
---
+++ store/fetcher/fetch-items.js
@@ -0,0 +1,21 @@
+import useSWR from 'swr';
+// Fetcher for the SWR state.
+async function fetcher(url) {
+  const response = await fetch(url);
+  const responseData = await response.json();
+  if (!response.ok) {
+    // Make SWR catch the failed response.
+    throw new Error(responseData.message);
+  }
+  return responseData;
+}
+
+export default function FetchItems(uri) {
+  const { data, error } = useSWR(uri, fetcher, { revalidateOnFocus: false });
+
+  return {
+    items: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+}
