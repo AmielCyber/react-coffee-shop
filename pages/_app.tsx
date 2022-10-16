845c48a73e98f270b0a17e09a9b06b38a37fae86
Amiel Nava <amieln@icloud.com>
2022-10-16 09:52:59 -0700

    Added type for session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
 import { LazyMotion, domAnimation } from "framer-motion";
+import { SessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
 import type { AppProps } from "next/app";
 // My imports.
 import HeadMeta from "../components/Layout/HeadMeta";
@@ -9,19 +10,22 @@ import Layout from "../components/Layout/Layout";
 // CSS style.
 import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ session: Session }>) {
   return (
     <>
       <HeadMeta />
-      <SessionProvider>
-        <Provider store={store}>
-          <LazyMotion features={domAnimation} strict>
+      <Provider store={store}>
+        <LazyMotion features={domAnimation} strict>
+          <SessionProvider session={pageProps.session}>
             <Layout>
               <Component {...pageProps} />
             </Layout>
-          </LazyMotion>
-        </Provider>
-      </SessionProvider>
+          </SessionProvider>
+        </LazyMotion>
+      </Provider>
     </>
   );
 }
