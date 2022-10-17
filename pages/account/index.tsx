d3171e5de35666235f13966106f2a67de2a463c6
Amiel Nava <amieln@icloud.com>
2022-10-17 16:50:41 -0700

    API page direction done in client side

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -1,19 +1,16 @@
-// Next.js Backend imports.
-import { GetServerSideProps } from "next";
-import { authOptions } from "../api/auth/[...nextauth]";
-import { unstable_getServerSession } from "next-auth/next";
 import Image from "next/image";
-// Frontend imports
 import dynamic from "next/dynamic";
 import { useState, Suspense } from "react";
 import Head from "next/head";
 import { m } from "framer-motion";
-import type { Session } from "next-auth";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 // My imports.
 import { pageAnimation } from "../../utils/animations/animation";
 import coffeeLove from "../../assets/coffeeLove.jpg";
 import Card from "../../components/UI/Card";
 import UserProfile from "../../components/Profile/UserProfile";
+import LoadingSpinner from "components/UI/LoadingSpinner";
 // CSS import.
 import styles from "./AccountPage.module.css";
 // My dynamic import.
@@ -22,82 +19,66 @@ const PastOrders = dynamic(
   { ssr: false }
 );
 
-type AccountPageProps = {
-  session: Session;
-};
-export default function AccountPage({ session }: AccountPageProps) {
+export default function AccountPage() {
+  const { data: session, status } = useSession();
   const [showPastOrders, setShowPastOrders] = useState(false);
+  const router = useRouter();
 
   const toggleShowPastOrdersHandler = () => {
     setShowPastOrders((prevState) => !prevState);
   };
 
-  return (
-    <>
-      <Head>
-        <title>View your account.</title>
-        <meta
-          name="description"
-          content="Change your password or view your past orders."
-          title="title"
-        />
-      </Head>
-      <div className={styles.accountPageImage}>
-        <Image
-          alt="A coffee cup in a white background and letters spelling LOVE"
-          src={coffeeLove}
-          title="Image by @inchristalone from Unsplash"
-          layout="fill"
-          objectFit="cover"
-          quality={80}
-          objectPosition="center"
-          placeholder="blur"
-        />
-      </div>
-      <m.div initial="in" animate="animate" variants={pageAnimation}>
-        <Card style="container">
-          <UserProfile session={session} />
-        </Card>
-        <div
-          className={styles.toggleShowOrders}
-          onClick={toggleShowPastOrdersHandler}
-          role="button"
-        >
-          <Card style="displayContainer">
-            <h2>{showPastOrders ? "Hide Past Orders" : "Show Past Orders"}</h2>
-          </Card>
+  if (status === "authenticated") {
+    return (
+      <>
+        <Head>
+          <title>View your account.</title>
+          <meta
+            name="description"
+            content="Change your password or view your past orders."
+            title="title"
+          />
+        </Head>
+        <div className={styles.accountPageImage}>
+          <Image
+            alt="A coffee cup in a white background and letters spelling LOVE"
+            src={coffeeLove}
+            title="Image by @inchristalone from Unsplash"
+            layout="fill"
+            objectFit="cover"
+            quality={80}
+            objectPosition="center"
+            placeholder="blur"
+          />
         </div>
-        {showPastOrders && (
-          <Suspense fallback={`Loading Past Orders...`}>
-            <PastOrders />
-          </Suspense>
-        )}
-      </m.div>
-    </>
-  );
-}
-
-// Protect API Route.
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  // Get current session if there is one.
-  const session = await unstable_getServerSession(
-    context.req,
-    context.res,
-    authOptions
-  );
-
-  if (session) {
-    return {
-      props: {
-        session,
-      },
-    };
+        <m.div initial="in" animate="animate" variants={pageAnimation}>
+          <Card style="container">
+            <UserProfile session={session} />
+          </Card>
+          <div
+            className={styles.toggleShowOrders}
+            onClick={toggleShowPastOrdersHandler}
+            role="button"
+          >
+            <Card style="displayContainer">
+              <h2>
+                {showPastOrders ? "Hide Past Orders" : "Show Past Orders"}
+              </h2>
+            </Card>
+          </div>
+          {showPastOrders && (
+            <Suspense fallback={`Loading Past Orders...`}>
+              <PastOrders />
+            </Suspense>
+          )}
+        </m.div>
+      </>
+    );
   }
-  // If there is no session prompt user to sign in.
-  return {
-    redirect: {
-      destination: "/auth",
-      permanent: false,
-    },
-  };
-};
+  if (status === "loading") {
+    return <LoadingSpinner />;
+  }
+  if (status === "unauthenticated") {
+    router.push("/");
+  }
+}
