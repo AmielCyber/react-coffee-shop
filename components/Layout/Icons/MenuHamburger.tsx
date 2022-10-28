6aa34aaf8d8f144c7ba39f706ec3c6a25e6b600e
Amiel Nava <amieln@icloud.com>
2022-10-28 12:20:15 -0700

    Hamburger button for opening the SideDrawer menu.

---
+++ components/Layout/Icons/MenuHamburger.tsx
@@ -0,0 +1,23 @@
+// Up-Round-Arrow obtained from https://iconoir.com
+function MenuHamburger() {
+  return (
+    <svg
+      width="36px"
+      height="36px"
+      strokeWidth="1.50"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      color="#000000"
+    >
+      <path
+        d="M3 5h18M3 12h18M3 19h18"
+        stroke="#FFFFFF"
+        strokeWidth="1.70"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  );
+}
+export default MenuHamburger;
