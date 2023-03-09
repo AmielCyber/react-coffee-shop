666ca01a1e9cb6fbaf4b14b1351458142a5bdf4a
Amiel Nava <amieln@icloud.com>
2023-03-08 21:14:52 -0800

    Updated some HTTP return codes

--- pages/api/user/change-password.ts
+++ pages/api/user/change-password.ts
@@ -1,4 +1,3 @@
-
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]";
@@ -12,91 +11,102 @@ import prisma from "../../../utils/db/prisma";
 const DEMO_EMAIL = "demo@gmail.com";
 // Validate if a request is validated or not
 // Protects API routes.
-export default async function handler(req: NextApiRequest, res: NextApiResponse){
-    if(req.method !== "PATCH"){
-        res.status(400).json({message: "Invalid call"});
-        return;
-    }
-    // Get current session.
-    const session = await getServerSession(req, res, authOptions);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "PATCH") {
+    res.status(403).json({ message: "Invalid call" });
+    return;
+  }
+  // Get current session.
+  const session = await getServerSession(req, res, authOptions);
 
-    // Check if client is authenticated
-    if(!session){
-        res.status(401).json({message: "Not authenticated!"});
-        return;
-    }
+  // Check if client is authenticated
+  if (!session) {
+    res.status(401).json({ message: "Not authenticated!" });
+    return;
+  }
 
-    // Get the user's credential
-    const userEmail: string = session.user.email;
-    const enteredOldPassword: string = req.body.currentPassword;
-    const enteredNewPassword: string = req.body.newPassword;
+  // Get the user's credential
+  const userEmail: string = session.user.email;
+  const enteredOldPassword: string = req.body.currentPassword;
+  const enteredNewPassword: string = req.body.newPassword;
 
-    // If user is the demo account then do not change password.
-    if(userEmail === DEMO_EMAIL){
-        // Guard the demo's account password.
-        res.status(403).json({
-            message: "Demo account can not change password. Please use another account to try out this function."
-        })
-        return;
-    }
+  // If user is the demo account then do not change password.
+  if (userEmail === DEMO_EMAIL) {
+    // Guard the demo's account password.
+    res.status(423).json({
+      message:
+        "Demo account can not change password. Please use another account to try out this function.",
+    });
+    return;
+  }
 
-    // Validate new password.
-    const passwordValidation: SafeParseReturnType<string,string> = validatePassword(enteredNewPassword);
-    if(!passwordValidation.success){
-        res.status(400).json({
-            message: passwordValidation.error.message,
-        });
-        return;
-    }
+  // Validate new password.
+  const passwordValidation: SafeParseReturnType<string, string> =
+    validatePassword(enteredNewPassword);
+  if (!passwordValidation.success) {
+    res.status(400).json({
+      message: passwordValidation.error.message,
+    });
+    return;
+  }
 
-    // Get the user's data from our db.
-    let user: User | null = null;
-    try {
-        user = await prisma.user.findUnique({
-            where: {
-                email: userEmail,
-            }
-        })
-    }catch(error){
-        res.status(500).json({
-            message: "Failed to connect to the user database!"
-        })
-        return;
-    }
+  // Get the user's data from our db.
+  let user: User | null = null;
+  try {
+    user = await prisma.user.findUnique({
+      where: {
+        email: userEmail,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to connect to the user database!",
+    });
+    return;
+  }
 
-    // Check if user exists
-    if(!user){
-        res.status(404).json({message: "User not found."})
-        return;
-    }
+  // Check if user exists
+  if (!user) {
+    res.status(404).json({ message: "User not found." });
+    return;
+  }
 
-    // Validate that entered password matches with the user's password.
-    const oldHashedPassword = user.password;
-    const passwordEqual = await verifyPassword(enteredOldPassword, oldHashedPassword);
+  // Validate that entered password matches with the user's password.
+  const oldHashedPassword = user.password;
+  const passwordEqual = await verifyPassword(
+    enteredOldPassword,
+    oldHashedPassword
+  );
 
-    // User entered invalid password.
-    if(!passwordEqual){
-        res.status(403).json({message: "Invalid current password entered."});
-        return;
-    }
+  // User entered invalid password.
+  if (!passwordEqual) {
+    res.status(403).json({ message: "Invalid current password entered." });
+    return;
+  }
 
-    // Hashed the newly created password to be stored in our DB.
-    const newHashedPassword = await hashPassword(enteredNewPassword);
+  // Hashed the newly created password to be stored in our DB.
+  const newHashedPassword = await hashPassword(enteredNewPassword);
 
-    // Update the password in our user's DB.
-    try {
-        await prisma.user.update({
-            where: {
-                email: userEmail,
-            },
-            data: {
-                password: newHashedPassword,
-            }
-        });
-      } catch (error) {
-            res.status(500).json({ message: "Server could not change password at the moment. Please try again later." });
-            return;
-      }
+  // Update the password in our user's DB.
+  try {
+    await prisma.user.update({
+      where: {
+        email: userEmail,
+      },
+      data: {
+        password: newHashedPassword,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message:
+        "Server could not change password at the moment. Please try again later.",
+    });
+    return;
+  }
 
-    res.status(200).json({ message: "Password updated!" });
+  res.status(200).json({ message: "Password updated!" });
 }
